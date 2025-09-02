bad9d0d301717fcd5cd7b8e96b633b84aac2974b
Felipe Alves <felipealves.santos93@gmail.com>
2025-09-02 20:59:11 -0300

    Implementa Componentes CardTema e ListaTema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,9 +21,8 @@ function Navbar() {
 
         <div className="flex gap-4">
           Postagens 
-          Temas 
-          Cadastrar 
           <Link to='/temas' className='hover:underline'>Temas</Link>
+          <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
           Perfil 
           <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
         </div>
