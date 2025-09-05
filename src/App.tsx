739fc1f12309f838fe6a3ea10bbb82fc244c8655
Felipe Alves <felipealves.santos93@gmail.com>
2025-09-05 12:26:26 -0300

    Implementa e estiliza o Componente Deletar Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 import DeletarTema from "./components/tema/deletartema/DeletarTema";
 import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
 import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
               <Route path="/postagens" element={<ListaPostagens />} />
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
